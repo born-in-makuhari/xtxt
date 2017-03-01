5cb8bd0f88dd960dfb553d82431572f66bb6a4bf
craftpaperbag <kazu11518@gmail.com>
2017-03-01 17:06:52 +0900

    file upload

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 // 追加 1
 var multer = require('multer');
 var storage = multer.diskStorage({
   // ファイルの保存先を指定
   destination: function (req, file, cb) {
-    cb(null, '')
+    cb(null, path.resolve('tmp'))
   },
   // ファイル名を指定(オリジナルのファイル名を指定)
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
+    cb(null, file.fieldname + '-' + Date.now())
   }
 })
 var upload = multer({ storage: storage })
 //
+console.log(path.resolve('tmp'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,7 +24,7 @@ router.get('/', function(req, res, next) {
 });
 
 // 追加 2
-router.post('/', upload.single('file'), function(req, res) {
+router.post('/', upload.single('file'),function(req, res) {
   res.json({ 'result': 'success!' });
 });
 // 
