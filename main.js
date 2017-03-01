e001229777c1df4e2c09b6e5af989b7be9b3d0e2
craftpaperbag <kazu11518@gmail.com>
2017-03-01 13:02:55 +0900

    main分割

--- main.js
+++ main.js
@@ -3,39 +3,14 @@ console.log('|  xtxt  |')
 console.log('|--------|')
 
 var parser = require('excel');
-var eaw = require('eastasianwidth');
+var xtxt = require('./xtxt')
+var readlineSync = require('readline-sync');
 
-parser('testbook.xlsx', function(err, data) {
+// get filename
+let filename = readlineSync.question('input filename: ');
+console.log('filename: ' + filename);
+  
+parser(filename, function(err, data) {
   if (err) throw err;
-
-  // console.log(data);
-  render(data);
+  xtxt.render(data);
 });
-
-
-function render(aa) {
-  console.log('render()');
-
-  // get max length (ml) of each col
-  var ml = [];
-  aa.forEach(function(r) {
-    r.forEach(function(cell, i) {
-      var l = eaw.length(cell);
-      if (ml[i] == null || ml[i] < l) {
-        ml[i] = l;
-      }
-    });
-  });
-  // join each row with spaces
-  var txt = "";
-  aa.forEach(function(r) {
-    r.forEach(function(cell, i) {
-      var l = eaw.length(cell);
-      var spaces = Array(ml[i] - l + 2 + 1).join(' '); // 余白2つ
-      txt = txt + cell + spaces;
-    });
-    txt = txt + "\n";
-  });
-
-  console.log(txt);
-}
---
+++ xtxt.js
@@ -0,0 +1,27 @@
+var parser = require('excel');
+var eaw = require('eastasianwidth');
+
+module.exports.render = function render(aa) {
+  // get max length (ml) of each col
+  var ml = [];
+  aa.forEach(function(r) {
+    r.forEach(function(cell, i) {
+      var l = eaw.length(cell);
+      if (ml[i] == null || ml[i] < l) {
+        ml[i] = l;
+      }
+    });
+  });
+  // join each row with spaces
+  var txt = "";
+  aa.forEach(function(r) {
+    r.forEach(function(cell, i) {
+      var l = eaw.length(cell);
+      var spaces = Array(ml[i] - l + 2 + 1).join(' '); // 余白2つ
+      txt = txt + cell + spaces;
+    });
+    txt = txt + "\n";
+  });
+
+  console.log(txt);
+}
