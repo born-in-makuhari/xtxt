362ef3d2051807f2f7e7be1fc34e8947502a3141
craftpaperbag <kazu11518@gmail.com>
2017-03-01 12:31:43 +0900

    半角対応

--- main.js
+++ main.js
@@ -15,6 +15,24 @@ parser('testbook.xlsx', function(err, data) {
 function render(aa) {
   console.log('render()');
 
-  
+  // get max length (ml) of each col
+  var ml = [];
+  aa.forEach(function(r) {
+    r.forEach(function(cell, i) {
+      if (ml[i] == null || ml[i] < cell.length) {
+        ml[i] = cell.length;
+      }
+    });
+  });
+  // join each row with spaces
+  var txt = "";
+  aa.forEach(function(r) {
+    r.forEach(function(cell, i) {
+      var spaces = Array(ml[i] - cell.length + 2 + 1).join(' '); // 余白2つ
+      txt = txt + cell + spaces;
+    });
+    txt = txt + "\n";
+  });
 
+  console.log(txt);
 }
